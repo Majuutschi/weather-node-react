a2b5cbb6c055eaba66f499cf6da4b2cf6a637631
Maja Garpelin <Majagarpelin@gmail.com>
2022-07-06 11:40:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Weather app
+
+Developed using React.js and Node.js
+
+## Node server
+
+Run with following command:
+
+### `cd data`
+### `npm i`
+### `node app.js`
+
+
+## React app
+
+Run with following command:
+
+### `cd app`
+### `npm i`
+### `npm start`
---
+++ data/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const PORT = 9999;
+
+const url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json';
+
