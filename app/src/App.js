bdf4d257d61a392195175c3b9cfb042fcb848339
Maja Garpelin <Majagarpelin@gmail.com>
2022-07-06 16:19:38 +0200

    getting started

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+  const [weatherData, setWeatherData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [temp, setTemp] = useState([]);
+  const [wind, setWind] = useState([]);
+
+
+  useEffect(() => {
+
+    const fetchWeatherDetails = async () => {
+      const response = await fetch(
+        `http://localhost:9999/getWeatherInfo/all`,
+        {
+          method: 'GET',
+          headers: new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json;charset=utf-8'
+          })
+        }
+      );
+
+      const postData = await response.json();
+      setWeatherData(postData.timeSeries[1]);
+      setIsLoading(false);
+      setTemp(postData.timeSeries[1].parameters[10].values[0]);
+      setWind(postData.timeSeries[1].parameters[14].values[0]);
+      console.log(postData.timeSeries[1].validTime)
+    };
+
+    fetchWeatherDetails()
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        isLoading &&
+        <p>Getting your weather details!</p>
+      }
+      {
+        !isLoading &&
+        <div>
+          <h4>The weather forecast for today at the Royal Palace in Stockholm</h4>
+          <p>Temperature: {temp} {weatherData.parameters[10].unit}</p>
+          <p>Wind: {wind} {weatherData.parameters[14].unit}</p>
+        </div>
+        
+      }
     </div>
   );
 }
 
-export default App;
+export default App
--- data/app.js
+++ data/app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const cors = require('cors');
 
 const PORT = 9999;
 
-const url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json';
+const URL = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json';
 
+app.use(cors());
+
+app.get('/getWeatherInfo/all/', async (req, res) => {
+
+  let result;
+
+  try {
+    result = await axios.get(URL);
+    res.json(result.data);
+  } catch (err) {
+    console.error(`failed all api due to ${err.message}`);
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`App running at http://localhost:${PORT}`);
+})
